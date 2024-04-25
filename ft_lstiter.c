23b2f7c72dfe816b1a366c20d530c2b00d087db0
diogooz-hub <112966199+diogooz-hub@users.noreply.github.com>
2024-04-25 15:53:10 +0100

    libft

---
+++ ft_lstiter.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpaco <dpaco@student.42lisboa.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/30 20:19:24 by dpaco             #+#    #+#             */
+/*   Updated: 2023/04/30 20:55:03 by dpaco            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*temp;
+
+	if (lst == NULL || f == NULL)
+		return ;
+	temp = lst;
+	while (temp != NULL)
+	{	
+		f(temp->content);
+		temp = temp->next;
+	}
+}
